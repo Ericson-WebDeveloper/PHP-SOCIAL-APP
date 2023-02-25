fffbab1c522f7e440f8088ddd51e86f45c9a4ac4
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-02-25 20:31:54 +0800

    Update class.php

--- class/class.php
+++ class/class.php
@@ -1,9 +1,9 @@
 <?php
 require_once(dirname(__FILE__, 2).'/vendor/autoload.php');
-require_once(dirname(__FILE__, 1).'./connect.db.php');
-require_once(dirname(__FILE__, 1).'./UserClass.php');
-require_once(dirname(__FILE__, 1).'./PostClass.php');
-require_once(dirname(__FILE__, 1).'./pusherNotif.php');
+require_once(dirname(__FILE__, 1).'/connect.db.php');
+require_once(dirname(__FILE__, 1).'/UserClass.php');
+require_once(dirname(__FILE__, 1).'/PostClass.php');
+require_once(dirname(__FILE__, 1).'/pusherNotif.php');
 
 $connect = new Connection();
 $user = new User($connect);
