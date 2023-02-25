02ed2c4dca9e5ddce85bae3e10cf3feb29af9ba5
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-02-25 21:57:02 +0800

    Update OnlineAlert.js

--- public/js/OnlineAlert.js
+++ public/js/OnlineAlert.js
@@ -1,8 +1,8 @@
 export default class OnlineNotification {
     constructor() {
         // super()
-        this.pusher = new Pusher('c77d753570287883aa81', {
-            cluster: 'ap1'
+        this.pusher = new Pusher('983513d8fdea2da8d62d', {
+            cluster: 'ap3'
         });
         
         this.channel = this.pusher.subscribe('online-alert-channel');
