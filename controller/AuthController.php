8a70f1f37085f36e22eba489d02e326c4e666517
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-02-25 20:27:08 +0800

    Update AuthController.php

--- controller/AuthController.php
+++ controller/AuthController.php
@@ -1,9 +1,7 @@
 <?php
 
-// require_once(dirname(__FILE__) . "/vendor/autoload.php");
-// require_once(dirname(__FILE__, 2) . "/class/class.php");
-echo dirname(__FILE__) . "/class/class.php";
-die();
+require_once(dirname(__FILE__, 2) . "/vendor/autoload.php");
+require_once(dirname(__FILE__, 2) . "/class/class.php");
 
 use Rakit\Validation\Validator;
 use Ramsey\Uuid\Uuid;
