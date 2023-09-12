36e0279577f99bae41c2df05b402e6bf0901aea4
Ericson-WebDeveloper <60337201+Ericson-WebDeveloper@users.noreply.github.com>
2023-09-12 16:26:44 +0800

    Update ImageHelper.js

--- public/js/ImageHelper.js
+++ public/js/ImageHelper.js
@@ -6,19 +6,6 @@ export const imageToBase64 = (element, callback) => {
 
     reader.onloadend = function() {
         callback(reader.result);
-        // console.log('RESULT', reader.result)
     }
     reader.readAsDataURL(file);
 }
-
-// export default class HelperClass {
-//     imageToBase64 = (element, callback) => {
-//         let file = element.files[0];
-//         let reader = new FileReader();
-
-//         reader.onloadend = function() {
-//             callback(reader.result);
-//         }
-//         reader.readAsDataURL(file);
-//     }
-// }
